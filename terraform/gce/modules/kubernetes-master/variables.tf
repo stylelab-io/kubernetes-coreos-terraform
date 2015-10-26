ac87e1427075bb78c1fc49e2e7f3a0487d225cbc
Steven Wirges <steven.wirges@stylelounge.de>
2015-10-26 14:24:17 +0100

    removed unused variable

--- terraform/gce/modules/kubernetes-master/variables.tf
+++ terraform/gce/modules/kubernetes-master/variables.tf
@@ -12,7 +12,6 @@ variable "gce_project" {}
 variable "gce_region" {}
 variable "gce_zone" {}
 variable "cluster_prefix" {}
-variable "gce_sshkey_metadata" {}
 variable "gce_account_file" {}
 variable "gce_network_range" {}
 
