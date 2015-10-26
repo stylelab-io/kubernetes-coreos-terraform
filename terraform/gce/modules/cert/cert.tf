11bca094ffd157aac298b7a1709ad7c022eed07e
Steven Wirges <steven.wirges@stylelounge.de>
2015-10-26 14:37:10 +0100

    enhanced cert generation

--- terraform/gce/modules/cert/cert.tf
+++ terraform/gce/modules/cert/cert.tf
@@ -1,5 +1,5 @@
 resource "execute_command" "ca_gen" {
-  command = "../../tools/etcd-ca --depot-path ${var.cert_path} init --passphrase ${var.cert_passphrase}"
+  command = "../../tools/etcd-ca --depot-path='${var.cert_path}' init --passphrase='${var.cert_passphrase}'"
   destroy_command = "rm -rf ${var.cert_path}"
 }
 
