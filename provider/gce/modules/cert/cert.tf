f2138a0c4e00bb9d6c144f4cbd37ea90c912b2c9
Steven Wirges <steven.wirges@stylelounge.de>
2016-01-04 15:02:43 +0100

    added project flag for gcloud commands to ensure it is uploading the files to the right project

--- provider/gce/modules/cert/cert.tf
+++ provider/gce/modules/cert/cert.tf
@@ -4,8 +4,8 @@ resource "execute_command" "ca_gen" {
 }
 
 resource "execute_command" "ca_to_metadata" {
-  command = "gcloud compute project-info add-metadata --metadata-from-file ${var.cluster_prefix}ca-crt-etcd=$PWD/${var.cert_path}/ca.crt"
-  destroy_command = "gcloud compute project-info remove-metadata --keys=${var.cluster_prefix}ca-crt-etcd"
+  command = "gcloud --project ${var.gce_project} compute project-info add-metadata --metadata-from-file ${var.cluster_prefix}ca-crt-etcd=$PWD/${var.cert_path}/ca.crt"
+  destroy_command = "gcloud --project ${var.gce_project} compute project-info remove-metadata --keys=${var.cluster_prefix}ca-crt-etcd"
 
   depends_on = [
       "execute_command.ca_gen"
@@ -13,8 +13,8 @@ resource "execute_command" "ca_to_metadata" {
 }
 
 resource "execute_command" "ca_key_to_metadata" {
-  command = "gcloud compute project-info add-metadata --metadata-from-file ${var.cluster_prefix}ca-key-etcd=$PWD/${var.cert_path}/ca.key"
-  destroy_command = "gcloud compute project-info remove-metadata --keys=${var.cluster_prefix}ca-key-etcd"
+  command = "gcloud --project ${var.gce_project} compute project-info add-metadata --metadata-from-file ${var.cluster_prefix}ca-key-etcd=$PWD/${var.cert_path}/ca.key"
+  destroy_command = "gcloud --project ${var.gce_project} compute project-info remove-metadata --keys=${var.cluster_prefix}ca-key-etcd"
 
   depends_on = [
       "execute_command.ca_gen",
@@ -23,8 +23,8 @@ resource "execute_command" "ca_key_to_metadata" {
 }
 
 resource "execute_command" "ca_info_to_metadata" {
-  command = "gcloud compute project-info add-metadata --metadata-from-file ${var.cluster_prefix}ca-info-etcd=$PWD/${var.cert_path}/ca.crt.info"
-  destroy_command = "gcloud compute project-info remove-metadata --keys=${var.cluster_prefix}ca-info-etcd"
+  command = "gcloud --project ${var.gce_project} compute project-info add-metadata --metadata-from-file ${var.cluster_prefix}ca-info-etcd=$PWD/${var.cert_path}/ca.crt.info"
+  destroy_command = "gcloud --project ${var.gce_project} compute project-info remove-metadata --keys=${var.cluster_prefix}ca-info-etcd"
 
   depends_on = [
       "execute_command.ca_gen",
--- provider/gce/modules/cert/variables.tf
+++ provider/gce/modules/cert/variables.tf
@@ -1,3 +1,5 @@
 variable "cert_path" {}
 variable "cert_passphrase" {}
 variable "cluster_prefix" {}
+variable "gce_project" {}
+variable "gce_account_file" {}
